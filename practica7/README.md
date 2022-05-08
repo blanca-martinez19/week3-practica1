5e56b35f16543efe49d5da244e0f072de8e71327
blanca21 <blancmart19@gmail.com>
2022-05-08 01:39:05 -0500

    Practica 7 - express api

---
+++ practica7/README.md
@@ -0,0 +1,2 @@
+# week3-practicas 7
+Practica 7 de semana 3 Backend LaunchX
---
+++ practica7/express_api/app.js
@@ -0,0 +1,58 @@
+// Usando objeto express
+const express = require("express");
+// App de Express
+const app = express();
+app.use(express.json()); // Indicamos que usaremos JSON
+// Puerto en que vamos a ver nuestra app: localhost:3000
+const port = 3000;
+
+//HTTP METHODS
+//GET LISTA
+app.get('/v1/explorers', (req, res) =>{
+    console.log(`Api Explorers GET ALL request ${new Date()}`);
+    
+    const explorer1 = {id:1, name:"Carlo1"};
+    const explorer2 = {id:2, name:"Carlo2"};
+    const explorer3 = {id:3, name:"Carlo3"};
+    const explorer4 = {id:4, name:"Carlo4"};
+
+    const explorers = [explorer1, explorer2, explorer3, explorer4];
+    res.status(200).json(explorers);
+});
+
+//GET ID
+app.get('/v1/explorers/:id', (req, res) =>{
+    console.log(`Api Explorers GET request ${new Date()}`);
+    console.log(`Getting explorer with id ${req.params.id}`);
+
+    const explorer = {id:1, name:"Carlo"};
+    res.status(200).json(explorer);
+});
+
+//POST
+app.post('/v1/explorers', (req, res)=>{
+    console.log(`Api Explorers POST request ${new Date()}`);
+    const requestBody = req.body //parametros de un cliente
+    res.status(201).json({message: "Created"});
+});
+
+//PUT
+app.put('/v1/explorers/:id', (req, res)=>{
+    console.log(`Api Explorers PUT request ${new Date()}`);
+    console.log(`Getting explorer with id ${req.params.id}`);
+    const requestBody = req.body //parametros de un cliente
+    res.status(200).json({message: "Updated!"});
+});
+
+//DELETE
+app.delete('/v1/explorers/:id', (req, res)=>{
+    console.log(`Api Explorers DELETE request ${new Date()}`);
+    console.log(`Getting explorer with id ${req.params.id}`);
+    const requestBody = req.body //parametros de un cliente
+    res.status(200).json({message: "Deleted!"});
+});
+
+// Con esto inicializamos esta app
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
